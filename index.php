1cf870472ab8f83bdecc764f2cc4565b72cf0ba2
Steve Stone <stoneflynm@gmail.com>
2017-08-06 20:56:54 -0600

    added onclick event

--- index.php
+++ index.php
@@ -17,6 +17,9 @@
 		<div id="future" onmouseenter="changeColor();">
 			<p>We want a shrubbery!! Ah, now we see the violence inherent in the system! Well, how'd you become king, then? Who's that then? We shall say 'Ni' again to you, if you do not appease us. Knights of Ni, we are but simple travelers who seek the enchanter who lives beyond these woods. Why do you think that she is a witch? I am your king. Listen. Strange women lying in ponds distributing swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony. He hasn't got shit all over him. Well, I didn't vote for you. Oh! Come and see the violence inherent in the system! Help, help, I'm being repressed! You can't expect to wield supreme power just 'cause some watery tart threw a sword at you! What do you mean? The Knights Who Say Ni demand a sacrifice! Well, we did do the nose. Well, I didn't vote for you. And the hat. She's a witch! Shut up! No, no, no! Yes, yes. A bit. But she's got a wart. Well, I didn't vote for you. The nose? Bloody Peasant!</p>
 		</div>
-		<div
+		<h2>Help Me</h2>
+		<div id="samuel" onclick="changeFont();">
+			<p>Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.</p>
+		</div>
 	</body>
 </html>
--- javascript/script.js
+++ javascript/script.js
@@ -6,3 +6,6 @@ function changeColor() {
 	document.getElementById("future").style.backgroundColor = "lightblue";
 }
 
+function changeFont() {
+	document.getElementById("samuel").style.color = "red";
+}
