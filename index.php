16e64b7c75fcff5d1579c012524487df1e0cd55b
Steve Stone <stoneflynm@gmail.com>
2017-08-09 22:33:47 -0600

    trying to still love bacon

--- index.php
+++ index.php
@@ -10,11 +10,14 @@
 		<!-- javascript -->
 		<script type="text/javascript" src="javascript/script.js"></script>
 
+		<!-- jQuery   -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+
 		<title>JavaScript Challenge</title>
 	</head>
 	<body>
 		<h1>Hello JavaScript</h1>
-		<div id="future" onmouseenter="changeColor();">
+		<div id="future" onmouseenter="changeColor();" onmouseleave="resetColor();">
 			<p>We want a shrubbery!! Ah, now we see the violence inherent in the system! Well, how'd you become king, then? Who's that then? We shall say 'Ni' again to you, if you do not appease us. Knights of Ni, we are but simple travelers who seek the enchanter who lives beyond these woods. Why do you think that she is a witch? I am your king. Listen. Strange women lying in ponds distributing swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony. He hasn't got shit all over him. Well, I didn't vote for you. Oh! Come and see the violence inherent in the system! Help, help, I'm being repressed! You can't expect to wield supreme power just 'cause some watery tart threw a sword at you! What do you mean? The Knights Who Say Ni demand a sacrifice! Well, we did do the nose. Well, I didn't vote for you. And the hat. She's a witch! Shut up! No, no, no! Yes, yes. A bit. But she's got a wart. Well, I didn't vote for you. The nose? Bloody Peasant!</p>
 		</div>
 		<h2>Help Me</h2>
@@ -22,15 +25,17 @@
 			<p>Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.</p>
 		</div>
 
-		<div>
-			<label for="boxContent">Say WAT</label>
-			<input type="text" id="boxContent">
-		</div>
-		<button id="btn" onclick="sayWhat();">Click Me</button>
-
-		<p id="help" onclick="changeText();">Click to Change My Text!</p>
-		
 		<img src="https://www.fillmurray.com/225/225" alt="bill murray" id="imgChange" onclick="changeImage();">
+
+		<div>
+			<h2>Who Loves Bacon?!!</h2>
+			<button id="getBacon" class="btn" onclick="jsBaconRequest();">I Love Bacon</button>
+			<div id="bacon"></div>
+		</div>
+
+
+		
+
 
 
 	</body>
--- javascript/script.js
+++ javascript/script.js
@@ -6,13 +6,15 @@ function changeColor() {
 	document.getElementById("future").style.backgroundColor = "lightblue";
 }
 
+function resetColor() {
+	document.getElementById("future").style.backgroundColor = "";
+}
+
 function changeFont() {
 	document.getElementById("samuel").style.color = "red";
 }
 
-function sayWhat() {
-	document.getElementById("btn").innerHTML = document.getElementById("boxContent").value;
-}
+
 
 function changeText() {
 	document.getElementById("help").innerHTML = "Readymade flexitarian cornhole lomo swag chicharrones distillery twee adaptogen meggings. Butcher hot chicken stumptown, glossier banjo trust fund 3 wolf moon schlitz skateboard fixie. Before they sold out hot chicken farm-to-table, 90's celiac cray paleo. Migas shabby chic put a bird on it pop-up, schlitz beard blue bottle occupy pabst bicycle rights truffaut snackwave vinyl health goth sartorial."
@@ -28,3 +30,61 @@ function changeImage() {
 		document.getElementById("imgChange").src = "https://www.fillmurray.com/225/225";
 	}
 }
+
+$(document).ready(function() {
+	$("#getBacon").click(function() {
+		$.ajax({
+			method: "GET",
+			url: "https://baconipsum.com/api/?type=meat-and-filler"
+		}).done(function(reply) {
+			$("#bacon").html(reply);
+			console.log("you like bacon!!!");
+		});
+	});
+});
+
+/**
+function jsBaconRequest() {
+
+	// URL of the API Endpoint
+	var requestUrl = "https://baconipsum.com/api/?type=meat-and-filler";
+
+	// create new request
+	var httpRequest = new XMLHttpRequest();
+
+	// if can't create httpRequest request, alert error message
+	if (!httpRequest) {
+		alert('Giving up :( Cannot create an XMLHTTP instance');
+		return false;
+	}
+
+	// run function that prints out response onreadystatechange
+	httpRequest.onreadystatechange = printJsBacon;
+
+	// open the request
+	httpRequest.open("GET", requestUrl, true);
+
+	// send the request
+	httpRequest.send();
+
+	// this function prints the bacon response to the screen
+	// or an error if an exception is thrown
+	function printJsBacon() {
+		try {
+			if(httpRequest.readyState === XMLHttpRequest.DONE) {
+				if (httpRequest.status === 200) {
+
+					//parse JSON
+					var bacon = JSON.parse(httpRequest.response);
+
+					//put parsed JSON on screen
+					document.getElementById("bacon").innerHTML = bacon;
+					console.log("you have Vanilla.js flavored bacon!");
+				}
+			}
+		} catch(e) {
+			alert("Exception: " + e.description);
+		}
+	}
+}
+ **/
